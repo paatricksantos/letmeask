a8de60d93a8914286726f788c147d20d2ba698a1
Patrick <patrick.crf22@gmail.com>
2021-06-21 11:10:13 -0300

    Primeiro commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Button } from './components/Button';
+
+function App() {
+  return (
+    <div>
+      <Button text={'Não clique'} />
+      <Button />
+      <Button />
+      <Button />
+    </div>
+  );
+}
+
+export default App;
